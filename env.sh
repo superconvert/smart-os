c98b7828c29105bcc51c104797e26388444a1a9e
superconvert <48017165+superconvert@users.noreply.github.com>
2022-07-27 13:06:23 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-
+  
 rm work/kernel_install/ work/glibc_install/ work/busybox_install/ -rf
-apt -y install gcc g++ make gawk bison libelf-dev
+
+if [ -f "/usr/bin/apt" ]; then
+  apt -y install gcc g++ make gawk bison libelf-dev
+fi
+
+if [ -f "/usr/bin/yum" ]; then
+  yum -y install gcc gcc-c++ make gawk bison elfutils-libelf
+fi
