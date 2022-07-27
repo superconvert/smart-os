1cf88370da636ca83110e31cf8f6fad61d1bc7a6
superconvert <48017165+superconvert@users.noreply.github.com>
2022-07-28 06:15:43 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,9 +3,9 @@
 rm work/kernel_install/ work/glibc_install/ work/busybox_install/ -rf
 
 if [ -f "/usr/bin/apt" ]; then
-  apt -y install gcc g++ make gawk bison libelf-dev bridge-utils qemu-system docker.io
+  apt -y install gcc g++ make autoconf automake gawk bison libelf-dev bridge-utils qemu-system docker.io
 fi
 
 if [ -f "/usr/bin/yum" ]; then
-  yum -y install gcc gcc-c++ make gawk bison elfutils-libelf bridge-utils qemu-img qemu-kvm qemu-kvm-tools docker
+  yum -y install gcc gcc-c++ make autoconf auotmake gawk bison elfutils-libelf bridge-utils qemu-img qemu-kvm qemu-kvm-tools docker
 fi
